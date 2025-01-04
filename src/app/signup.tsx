40b93d763561062771eb3b592a3dfb458967400e
Bernardo Souza <bernardo.sfs27@gmail.com>
2025-01-04 13:54:46 -0300

    Implementando navegação de forma imperativa para o botão de voltar

--- src/app/signup.tsx
+++ src/app/signup.tsx
@@ -1,30 +1,43 @@
-import { Link } from "expo-router";
-import {View, Text, StyleSheet} from "react-native";
+import { router } from "expo-router";
+import { View, Text, StyleSheet, TouchableOpacity, Alert } from "react-native";
 
 
 //Este é o entrypoint de nossa aplicação
-export default function SignUp(){
-    return(
+export default function SignUp() {
+
+    function back() {
+
+        if(router.canGoBack()){
+            return Alert.alert("Não é possível voltar a página")
+        }
+
+        router.back();
+    }
+
+
+    return (
         <View style={styles.container}>
             <Text style={styles.title}>Criar Conta</Text>
-            <Link href="/" style={styles.back}>
-                Voltar
-            </Link>
+            <TouchableOpacity onPress={back}>
+                <Text style={styles.back}>
+                    Voltar
+                </Text>
+            </TouchableOpacity>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center'
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
     },
-    title:{
-        fontSize:22,
-        fontWeight:'bold'
+    title: {
+        fontSize: 22,
+        fontWeight: 'bold'
     },
-    back:{
-        fontSize:16,
+    back: {
+        fontSize: 16,
     }
 });
